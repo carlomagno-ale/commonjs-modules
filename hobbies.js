209caf61edc861a2ac0f7618404f3a5f705558bc
carlomagno-ale <carlomagno.ale@gmail.com>
2025-03-10 15:49:32 +0100

    add hobies function

---
+++ hobbies.js
@@ -0,0 +1,7 @@
+//Crea una funzione che accetta tre parametri: hobbyOne, hobbyTwo, hobbyThree. La funzione dovrebbe restituire un array di stringhe contenenti gli hobby.
+
+function hobbies(hobbyOne, hobbyTwo, hobbyThree) {
+    return [hobbyOne, hobbyTwo, hobbyThree];
+}
+
+module.exports = hobbies;
